ec791245acf1e19ac724c93452bea291be86b61a
caydenfreeze28! <225117382+caydenfreeze28@users.noreply.github.com>
2026-02-26 17:02:12 -0700

    complete

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02/24/2026 09:47:08 AM
+-- Create Date: 02/24/2026 09:44:49 AM
 -- Design Name: 
 -- Module Name: sevenseg_decoder_tb - Behavioral
 -- Project Name: 
@@ -30,14 +30,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
+ 
 entity sevenseg_decoder_tb is
 --  Port ( );
 end sevenseg_decoder_tb;
-
+ 
 architecture Behavioral of sevenseg_decoder_tb is
+    
+    signal w_Hex : STD_LOGIC_VECTOR (3 downto 0);
+    signal w_seg_n : STD_LOGIC_VECTOR (6 downto 0);
 
 begin
+    uut: entity work.sevenseg_decoder
+    port map (
+        i_Hex => w_Hex,
+        o_seg_n => w_seg_n
+    );
 
-
+    stim_proc: process
+    begin
+        w_Hex <= x"0"; wait for 10 ns;
+            assert (w_seg_n = "1000000") report "bad" severity failure;
+        w_Hex <= x"1"; wait for 10 ns;
+        w_Hex <= x"2"; wait for 10 ns;
+        w_Hex <= x"3"; wait for 10 ns;
+        w_Hex <= x"4"; wait for 10 ns;
+        w_Hex <= x"5"; wait for 10 ns;
+        w_Hex <= x"6"; wait for 10 ns;
+        w_Hex <= x"7"; wait for 10 ns;
+        w_Hex <= x"8"; wait for 10 ns;
+        w_Hex <= x"9"; wait for 10 ns;
+        w_Hex <= x"A"; wait for 10 ns;
+        w_Hex <= x"B"; wait for 10 ns;
+        w_Hex <= x"C"; wait for 10 ns;
+        w_Hex <= x"D"; wait for 10 ns;
+        w_Hex <= x"E"; wait for 10 ns;
+        w_Hex <= x"F"; wait for 10 ns;
+        wait; 
+    end process;
 end Behavioral;
